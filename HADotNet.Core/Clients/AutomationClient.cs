9992aa2d56c87f295ea0fc2259e27f186b3e0fa6
Amadeo Alex <68441479+amadeo-alex@users.noreply.github.com>
2024-07-13 11:11:03 +0200

    modifier order

--- HADotNet.Core/Clients/AutomationClient.cs
+++ HADotNet.Core/Clients/AutomationClient.cs
@@ -41,6 +41,6 @@ namespace HADotNet.Core.Clients
         /// </summary>
         /// <param name="id">The automation id.</param>
         /// <returns>The <see cref="AutomationResultObject"/>.</returns>
-        public new async Task<AutomationResultObject> Delete(string id) => await Delete<AutomationResultObject>($"/api/config/automation/config/{id}");
+        public async new Task<AutomationResultObject> Delete(string id) => await Delete<AutomationResultObject>($"/api/config/automation/config/{id}");
     }
 }
